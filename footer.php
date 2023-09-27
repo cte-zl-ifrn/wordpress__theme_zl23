c5fe4d668a52b772e9f8b4d3be86abad4ed21137
matheu46 <mathias.matheus@ymail.com>
2023-09-27 14:58:42 -0300

    #6 #7 header e footer no padrão do wordpress

---
+++ footer.php
@@ -0,0 +1,26 @@
+<footer id="mainFooter">
+        <div id="contactArea">
+            <p>
+                <span>Contatos</span> - (84) 3092-8900 | (84) 30928902
+            </p>
+            <p id="addressFooter">Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Norte ©2011.
+                Todos os direitos reservados. Av. Senador Salgado Filho, 1559 - Natal / RN CEP: 59015-000</p>
+        </div>
+        <div class="secondColun">
+            <section id="footerIcons">
+                <a href="https://www.facebook.com/ifrnzonaleste">
+                    <i class="fa-brands fa-square-facebook" alt="incone facebook"></i>
+                </a>
+                <a href="https://www.instagram.com/ifrnzonaleste/">
+                    <i class="fa-brands fa-instagram" alt="icone facebook ifrn-zl"></i>
+                </a>
+                <a href="https://www.youtube.com/c/ifrncampusead">
+                    <i class="fa-brands fa-youtube"></i>
+                </a>
+            </section>
+        </div>
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+
+</html>
