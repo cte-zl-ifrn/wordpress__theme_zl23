d402529cd5061fcdc50adf215a940234a084cc47
Matheu46 <mathias.matheus@ymail.com>
2023-10-15 23:41:26 -0300

    js do tema em functions

--- functions.php
+++ functions.php
@@ -4,13 +4,13 @@
 
 function importar_arquivos() {
     // CSS
-    wp_enqueue_style('estilos', get_template_directory_uri() . '/assets/css/all.css', array(), '1.0.0', 'all');
     wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css', array(), '6.4.2', 'all');
     wp_enqueue_style('open-sans', 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&family=Roboto:wght@100;500;700&display=swap', array(), '1.0.0', 'all');
+    wp_enqueue_style('estilos', get_template_directory_uri() . '/assets/css/all.css', array(), '1.0.0', 'all');
+
+    //JS
+    wp_enqueue_script('main_script', get_template_directory_uri() . '/assets/js/index.js', array('jquery'),'1.0', true);
     
-    // JS
-    // wp_enqueue_script('arquivo-js', get_template_directory_uri() . '/assets/js/test.js', array('jquery'), '1.0.0', true);
-                                                                    /* jquery carrega primeiro, true -> carrega no final */
 }
 
 add_action('wp_enqueue_scripts', 'importar_arquivos');
