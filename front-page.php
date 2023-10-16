3f27aa0c614054d3270a145945bc202b539ba5f9
Matheu46 <mathias.matheus@ymail.com>
2023-10-16 20:05:40 -0300

    #11 estrutura de loop para puxar os posts da categoria notícia

--- front-page.php
+++ front-page.php
@@ -62,4 +62,27 @@
   </aside>
 </section>
 
+<!-- Notícias -->
+<?php
+$count = 0;
+while(have_posts()) {
+  the_post(); 
+  if(has_term('noticias', 'category')) {
+  ?>
+    <div class="post-item">
+      <a href="<?php the_permalink(); ?>">
+        <?php if (has_post_thumbnail() && $count < 2) { ?>
+          <div>
+            <?php the_post_thumbnail('thumbnail'); ?>
+          </div>
+        <?php }
+        $count++;
+        ?>
+        <p><?php the_time('d \d\e F \d\e Y'); ?></p>
+        <h2><?php the_title(); ?></h2>
+      </a>
+    </div>  
+    <hr>
+<?php }} ?>
+
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -13,5 +13,9 @@ function importar_arquivos() {
     
 }
 
-add_action('wp_enqueue_scripts', 'importar_arquivos');
+function theme_setup() {
+    add_theme_support('post-thumbnails');
+}
 
+add_action('wp_enqueue_scripts', 'importar_arquivos');
+add_action('after_setup_theme', 'theme_setup');
