3b9beab0aaf882ccc775b6cde999e38f701a9106
ElieldsonKervem <kervem.elieldson@academico.ifrn.edu.br>
2023-10-19 18:48:30 -0300

    novo padrão adicionado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,87 +1,21 @@
-const institucionalLink = document.querySelector('#institucionalLink');
-const institucionalSubMenu = document.querySelector('#institucionalSubMenu');
-const menuItem = document.querySelectorAll('.menuItem');
-const closerDropMenu = document.querySelector('#closerDropMenu');
-const submenuUl = document.querySelectorAll('.submenuUl');
-const backMenuIcon = document.querySelectorAll('.backMenuIcon');
-const ensinoSubMenu = document.querySelector('#ensinoSubMenu');
-const ensinolink = document.querySelector('#ensinolink');
-const pesquisaLink = document.querySelector('#pesquisaLink');
-const pesquisaSubMenu = document.querySelector('#pesquisaSubMenu');
 
 
+const menuWithSubmenuItems = document.querySelectorAll(".menuwithsubmenu");
+const backMenuIcon = document.querySelectorAll(".backMenuIcon");
+const menulink = document.querySelectorAll(".menulink");
 
-function hideMenu(){
-    menuItem.forEach((item) => {
-        item.classList.remove('displayNone')
-    })
-    submenuUl.forEach((item) => {
-        item.classList.add('displayNone')
-    }
-    )
-}
+console.log(menuWithSubmenuItems);
+console.log(backMenuIcon);
 
-function subMenuInstitucional(){
-    menuItem.forEach((item) => {
-        item.classList.add('displayNone')
-    })
-    institucionalSubMenu.classList.remove('displayNone');
-   
-
-}
-function subMenuEnsino(){
-    menuItem.forEach((item) => {
-        item.classList.add('displayNone')
-    })
-    ensinoSubMenu.classList.remove('displayNone');
-   
-}
-
-function bachToMainMenu() {
-    
-    submenuUl.forEach((item) => {
-        item.classList.add('displayNone');
+menuWithSubmenuItems.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        e.target.nextElementSibling.classList.add("active");
+        e.target.nextElementSibling.classList.remove("displayNone");
+        menulink.forEach((link) => {
+            link.classList.add("displayNone");
+        }
+        );
     });
-
-   
-    menuItem.forEach((item) => {
-        item.classList.remove('displayNone');
-    });
-}
-
-function pesquisaSubMenuFunction(){
-    menuItem.forEach((item) => {
-        item.classList.add('displayNone')
-    })
-    pesquisaSubMenu.classList.remove('displayNone');
-   
-}
-
-pesquisaLink.addEventListener('click', (e) => {
-    pesquisaSubMenuFunction()
-})
-
-
-
-
-closerDropMenu.addEventListener('click', (e) => {
-    hideMenu();
-})
-
-institucionalLink.addEventListener('click', (e) => {
-    subMenuInstitucional()
 });
-
-
-ensinolink.addEventListener('click', (e) => {
-    subMenuEnsino()
-})
-
-
-backMenuIcon.forEach((item) => {
-    item.addEventListener('click', (e) => {
-        bachToMainMenu()
-    })
-})
 
 
