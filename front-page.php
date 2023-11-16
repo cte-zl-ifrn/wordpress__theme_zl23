c13577a75c96da5718a4ac64dd84e234d3341778
matheu46 <mathias.matheus@ymail.com>
2023-11-16 17:59:17 -0300

    slides página inicial

--- front-page.php
+++ front-page.php
@@ -1,10 +1,30 @@
 <?php get_header(); ?>
 
-<?php showSlideMenu(); ?>
+<!-- Slides -->
+<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+<?php foreach((array) getSlideMenu() as $menu_item): ?>
+      <div class="carousel-item active bg-black" data-bs-interval="10000">
+        <a href="<?php echo $menu_item->post_excerpt ?>">
+          <p><?php echo $menu_item->post_title ?></p>
+          <img src="<?php echo get_the_post_thumbnail_url($menu_item->ID, 'full'); ?>" class="d-block w-100" alt="...">
+        </a>
+      </div>
+<?php endforeach ?>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
 
 <!-- Atalhos -->
 <div>
-<?php foreach ((array) getshotcutMenu()as $menu_item ): ?>  
+<?php foreach ((array) getshotcutMenu() as $menu_item ): ?>  
     <a href='<?php echo $menu_item->url ?>'>
       <div>
         <?php echo formatShotcutMenu($menu_item->post_title) ?>
--- functions.php
+++ functions.php
@@ -3,18 +3,23 @@
 // função para importar arquivos de mídia (css, js)
 function importar_arquivos() {
     // CSS
+    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css', array(), '5.3.2', 'all');
     wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css', array(), '6.4.2', 'all');
     wp_enqueue_style('open-sans', 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&family=Roboto:wght@100;500;700&display=swap', array(), '1.0.0', 'all');
     wp_enqueue_style('custom-style', get_template_directory_uri() . '/caminho-para-seu-arquivo.css');
     wp_enqueue_style('estilos', get_template_directory_uri() . '/assets/css/all.css', array(), null, 'all');
 
     //JS
-    wp_enqueue_script('main_script', get_template_directory_uri() . '/assets/js/index.js', array('jquery'),null, true);
+    wp_enqueue_script('bootstrap-bundle', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js', array('jquery'), '5.3.2', true);
+    wp_enqueue_script('popper', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js', array(), '2.11.8', true);
+    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js', array('popper'), '5.3.2', true);
+    wp_enqueue_script('main_script', get_template_directory_uri() . '/assets/js/index.js', array('jquery'), null, true);
     
 }
 
 function theme_setup() {
-    add_theme_support('title-tag','post-thumbnails');
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
     register_nav_menus(
         [
             'mainMenu' => 'Principal',
@@ -54,7 +59,7 @@ function getGovMenu() {
     return wp_get_nav_menu_items( $menu->term_id, ['update_post_term_cache' => false, 'orderby'=>'menu_order'] );
 }
 
-function showSlideMenu() {
+function getSlideMenu() {
 	$locations = get_nav_menu_locations();
     $menu = wp_get_nav_menu_object( $locations[ "slideMenu" ] );
 
@@ -62,40 +67,19 @@ function showSlideMenu() {
 		return false;
 	}
 
-    $menu_items = wp_get_nav_menu_items( $menu->term_id, ['update_post_term_cache' => false, 'orderby'=>'menu_order'] );
-
-    echo '<section id="slideMenu" class="carousel" aria-label="Gallery">';
-
-        echo '<ol class="carousel__viewport">';
-        foreach ( (array) $menu_items as $menu_item ) {
-            echo "<li id='carousel__slide1'
-                        tabindex='0'
-                        class='carousel__slide'>
-                        <img src='$menu_item->post_content'>
-                    <span>$menu_item->attr_title</span>
-                    <div class='carousel__snapper'>
-                        <a href='#carousel__slide4'
-                        class='carousel__prev'>Go to last slide</a>
-                        <a href='#carousel__slide2'
-                        class='carousel__next'>Go to next slide</a>
-                    </div>
-                    </li>
-                    ";
+    $result = [];
+    foreach ((array) wp_get_nav_menu_items( $menu->term_id, ['update_post_term_cache' => true, 'orderby'=>'menu_order'] ) as $menu_item ) {
+        if ( ($menu_item->object != 'slideMenu') && (!has_post_thumbnail($menu_item->object_id)) ) {
+            continue;
         }
-        echo "</ol>";
+        $parts = explode('|', $menu_item->post_title);
+        $post = get_post($menu_item->object_id);
+        $post->post_thumbnail = get_the_post_thumbnail($menu_item->object_id);
+        // echo "<pre>";var_dump($post);die();
+        array_push($result, $post);
+    }
 
-        echo '<aside class="carousel__navigation">';
-            echo '<ol class="carousel__navigation-list">';
-                foreach ( (array) $menu_items as $menu_item ) {
-                    echo '<li class="carousel__navigation-item">
-                    <a href="#carousel__slide1"
-                    class="carousel__navigation-button">Go to slide 1</a>
-                </li>';
-                }
-            echo "</ol>";
-        echo "</aside>";
-
-    echo "</section>"; 
+    return $result;
 }
 
 function getShotcutMenu() {
@@ -189,5 +173,31 @@ function wporg_custom_post_type() {
 			'rewrite'     => array( 'slug' => 'featured-menu' ), // my custom slug
 		)
 	);
+    register_post_type('courses',
+        array(
+            'labels'      => array(
+                'name'          => __( 'Cursos', 'textdomain' ),
+                'singular_name' => __( 'Curso', 'textdomain' ),
+            ),
+            'public'      => true,
+            'has_archive' => true,
+            'exclude_from_search' => true,
+            'publicly_queryable' => false,
+            'show_ui' => true,
+            'supports' => [
+                'title',
+                'excerpt',
+                'thumbnail',
+                // 'editor',
+                // 'revisions',
+                // 'trackbacks',
+                // 'author',
+                // 'page-attributes',
+                // 'custom-fields', 
+                // 'post-formats'
+            ],
+            'rewrite'     => array( 'slug' => 'courses' ), // my custom slug
+        )
+    );
 }
 add_action('init', 'wporg_custom_post_type');
