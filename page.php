d9f25b3990764752b48ab2c51afd74b809036ba0
Matheu46 <mathias.matheus@ymail.com>
2023-11-21 23:17:44 -0300

    #13

--- page.php
+++ page.php
@@ -1,10 +1,12 @@
 <?php get_header(); 
 
-    while(have_posts()) {
-        the_post(); ?>
-        <h2><?php the_title(); ?></h2>
-        <p><?php the_content(); ?></p>
+while(have_posts()) {
+    the_post(); ?>
+    <section id="page">
+        <h1 class="first-title"><?php the_title(); ?></h1>
+        <p class="text"><?php the_content(); ?></p>
+    </section>
 
-    <?php } ?>
+<?php } ?>
 
 <?php get_footer(); ?>
