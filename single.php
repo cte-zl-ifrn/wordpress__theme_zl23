1958cf46842bb5a1533b619823f636a5b13e907a
Matheu46 <mathias.matheus@ymail.com>
2023-11-21 23:18:44 -0300

    #12

--- single.php
+++ single.php
@@ -1,10 +1,29 @@
 <?php get_header(); 
 
-    while(have_posts()) {
-        the_post(); ?>
-        <h2><?php the_title(); ?></h2>
+while(have_posts()) {
+    the_post(); ?>
+    <section id = "post">
+        <h1 class="post-title"><?php the_title(); ?></h1>
+        <span><?php the_time('d \d\e F \d\e Y'); ?></span>
         <p><?php the_content(); ?></p>
-
-    <?php } ?>
+        <div class="share-post">
+            <span>Compartilhe</span>
+            <div class="share-grid">
+                <div>
+                    <i class="fa-brands fa-whatsapp" alt="Whatsapp "></i>
+                </div>
+                <div>
+                    <i class="fa-brands fa-instagram" alt="Instagram"></i>
+                </div>
+                <div>
+                    <i class="fa-brands fa-facebook" alt="Facebook "></i>
+                </div>
+                <div>
+                    <i class="fa-brands fa-twitter" alt="Twitter"></i>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php } ?>
 
 <?php get_footer(); ?>
